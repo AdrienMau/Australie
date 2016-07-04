c8a47cf08361c4b8de11c4e49062660b3f485f4c
SmartiesJaune <adrienmau@orange.fr>
2016-07-04 15:14:50 +1000

    matlab

---
+++ Matlab/traitement1.m
@@ -0,0 +1,5 @@
+% C:\Users\Adrien\Documents\GitHub\Australie\30juin-MembraneAg\Images BMP\fprectangle-20Loop-6.99pA-DD0.1-LD20000-AD1000resol.bmp
+img=imread(
+imshow(img)
+% img2=fftshift(fft2(img));
+% imshowf(img2)
